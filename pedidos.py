cd791439cd39dcf7cfe96170977edf03c43b8431
Gabrielli <gabrielliborba42@gmail.com>
2026-03-26 08:35:09 -0300

    feat: cria classes Pedido e Produto

---
+++ pedidos.py
@@ -0,0 +1,5 @@
+
+class Pedidos:
+    def __init__(self, data, valorTotal):
+        self.data = data
+        self.valorTotal = valorTotal
---
+++ produto.py
@@ -0,0 +1,7 @@
+
+class Produto:
+    def __init__(self,nome,preco,estoque):
+        self.nome = nome
+        self.preco = preco
+        self.estoque = estoque
+        

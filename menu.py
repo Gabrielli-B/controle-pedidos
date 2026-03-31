6623607b679ff5d1321bb6209a78b646b476f7a2
Gabrielli <gabrielliborba42@gmail.com>
2026-03-31 07:58:24 -0300

    fix: ajusta menuAtualizar e menuRemover corrigindo validações e query SQL

--- menu.py
+++ menu.py
@@ -43,33 +43,53 @@ def menuListar():
     vsql="SELECT * FROM tb_produtos"
     res=manipulacaoBanco.consultar(vcon,vsql)
     for r in res:
-        print("ID:{r[0]}")
-        print("Nome:{r[1]}")
-        print("Preço R$:{r[2]}")
-        print("Estoque:{r[3]}")
+        print(f"ID:{r[0]}")
+        print(f"Nome:{r[1]}")
+        print(f"Preço R$:{r[2]}")
+        print(f"Estoque:{r[3]}")
         print("-" *30)
 
 
 def menuAtualizar():
     vid=int(input("Digite o ID do registro a ser Atualizado:"))
-    r=manipulacaoBanco.consultar(vcon,"SELECT * FROM tb_produtos WHERE ID_PRODUTO="+vid)
+    vsql="SELECT *FROM tb_produtos WHERE ID_PRODUTO=?"
+    r=manipulacaoBanco.consultar(vcon,vsql,(vid,))
+
+    if len(r)==0:
+        print("Produto não encontrado!")
+        return
+    
     rnome=r[0][1]
     rpreco=r[0][2]
     restoque=r[0][3]
 
     vnome=input("Digite o nome do produto:")
-    vpreco=float(input("Informe o preço do produto:"))
-    vestoque=int(input("Informe o estoque:"))
+    vpreco=input("Informe o preço do produto:")
+    vestoque=input("Informe o estoque:")
 
-    if(len(vnome)==0):
+    if vnome =="":
         vnome=rnome
-    if(len(vpreco)==0):
+    if vpreco =="":
         vpreco=rpreco
-    if(len(vestoque)==0):
-        vestoque=restoque
+    else:
+        vpreco=float(vpreco)
+    
+    if vestoque=="":
+       vestoque=restoque
+    else:
+        vestoque=int(vestoque)
 
-    vsql="UPDATE tb_produtos SET NOME_PRODUTO='"+vnome+"', PRECO_PRODUTO'"+vpreco+"', ESTOQUE_PRODUTO'"+vestoque+"'"
-    manipulacaoBanco.query(vcon,vsql)
+    vsql="UPDATE tb_produtos SET NOME_PRODUTO=?, PRECO_PRODUTO=?, ESTOQUE_PRODUTO=? WHERE ID_PRODUTO=?"
+
+    dados=(vnome,vpreco,vestoque,vid)
+    manipulacaoBanco.query(vcon,vsql,dados)
+    
+
+def menuRemover():
+    vid=int(input("Digite o ID do registro a ser deletado: "))
+    vsql="DELETE FROM tb_produtos WHERE ID_PRODUTO =?"
+    manipulacaoBanco.query(vcon,vsql,(vid,))
+    print("Produto removido!")
 
 op=0
 
@@ -83,5 +103,7 @@ while(op != 8):
         menuListar()
     elif(op==3):
         menuAtualizar()
+    elif(op==4):
+        menuRemover()
 
 vcon.close()       
